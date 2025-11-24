7e40351694493e53c9ea7b56f9cc1441ebb21fd5
Buddhika-W-Intelleon <buddhika.w@intelleon.com>
2025-11-24 17:19:07 +0530

    Packaging step done - need download

---
+++ electron-wrapper/electron-main.ts
@@ -0,0 +1,28 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+let mainWindow: BrowserWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // Load the React app (after build)
+  mainWindow.loadFile(path.join(__dirname, 'frontend/dist/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
